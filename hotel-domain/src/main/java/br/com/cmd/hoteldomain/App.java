adf0339108b49e8be30adf1dcc04a716651cb1b7
Alex Porto <alexporto90@gmail.com>
2012-04-18 10:53:09 -0300

    Ok agora vai!

---
+++ hotel-domain/src/main/java/br/com/cmd/hoteldomain/App.java
@@ -0,0 +1,13 @@
+package br.com.cmd.hoteldomain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
