55a1b45ec79b3ea758a72da536724065ab8c85ab
Alex Porto <alexporto90@hotmail.com>
2012-04-19 15:30:35 -0300

    inicio de teste

---
+++ hotel/hotel-domain/src/test/java/br/com/cmd/hoteldomain/usuario.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.*;
+
+/**
+ *
+ * @author AlexPorto
+ */
+public class usuario {
+    
+    public usuario() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+     @Test
+     public void hello() {
+     Boolean hello;
+     
+     }
+}
